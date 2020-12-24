ea42926531478d268c7226f86e6d92f50803d22e
Mohammad Hassan <rhassan1@live.com>
2020-12-23 21:45:13 -0600

    mohammad- added modal for adding new links

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import Header from "./Header";
 
 import LinkList from './LinkList';
+import Linkmodal from "./Linkmodal";
 
 
 const App = () => {
     return (
     <>
     <Header />
-  
+    <Linkmodal />
     <LinkList />
     </>
     );
