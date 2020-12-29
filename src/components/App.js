2d240a599d6003b24d8728caa94b9b9089a9a848
Mohammad Hassan <rhassan1@live.com>
2020-12-29 16:53:00 -0600

    mohammad- added create and edit modals. Don't know if they work

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,51 @@ import Header from "./Header";
 import SearchBar from "./SearchBar";
 import LinkList from './LinkList';
 import Linkmodal from "./Linkmodal";
+import { LinkedIn } from "@material-ui/icons";
 
 const App = () => {
+  const [postModal, setPostModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
+  const [linkID, setLinkID] = useState(null);
+  const [linkComment, setLinkComment] = useState(null);
+  const [linkCount, setLinkCount] = useState(null);
+  const [links, setLinks] = useState([]); //list of all links
+  console.log(links);
+  
+  useEffect(() => {
+    hitAPI("GET", "links")
+    .then((data) => {
+      setLinks(data);
+    })
+    .catch(console.error);
+  }, []);
+
   return (
     <>
     <Header />
-    <SearchBar />
-    <Linkmodal />
-    <LinkList />
+    <div className="search-post">
+      <SearchBar />
+      <Button
+      variant="contained"
+      color="primary"
+      onClick={setPostModal(true)}>New Link</Button>
+    </div>
+    <Linkmodal
+      postModal={postModal}
+      setPostModal={setPostModal}
+      editModal={editModal}
+      setEditModal={setEditModal}
+      linkID={linkID}
+      setLinkID={setLinkID}
+      linkComment={linkComment}
+      setLinkComment={setLinkComment}
+      linkCount={linkCount}
+      setLinkCount={setLinkCount} />
+    <LinkList
+      setEditModal={setEditModal}
+      setLinkID={setLinkID}
+      setLinkComment={setLinkComment}
+      setLinkCount={setLinkCount} />
     </>
     );
   };
--- src/components/Linkmodal.js
+++ src/components/Linkmodal.js
@@ -3,15 +3,28 @@ import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
 import hitAPI from '../api/index';
 
-const Linkmodal = () => {
-  const [showModal, setShowModal] = useState(false);
+const Linkmodal = ({
+  postModal,
+  setPostModal,
+  editModal,
+  setEditModal,
+  linkID,
+  setLinkID,
+  linkComment,
+  setLinkComment,
+  linkCount,
+  setLinkCount
+}) => {
   const [link, setLink] = useState("");
   const [comment, setComment]= useState("");
   const [tags, setTags] = useState([]);
 
-  function clearInputs() {
+  function clear() {
     setLink("");
     setComment("");
+    setLinkComment(null);
+    setLinkCount(null)
+    setLinkID(null);
     setTags([]);
   }
   // const [links, setLinks] = useState([]);
@@ -27,15 +40,27 @@ const Linkmodal = () => {
   
   return (
     <>
-    <Button
-      variant="contained"
-      color="primary"
-      onClick={setShowModal(true)}>New Link</Button>
-      {showModal ? (
-        <Modal className="modal">
+      {postModal ? (
+        <Modal className="post-modal">
           <h2>Create a link</h2>
           <div className="form">
-            <form>
+            <form classname="post-form"
+              onSubmit={event => {
+                event.preventDefault();
+
+                const body = {
+                  link,
+                  comment
+                }
+
+                hitAPI("POST", "links", body)
+                  .then((data) => {
+                    console.log("post successful!")
+                    console.log(data)
+                    clearInputs();
+                    setPostModal(false);
+                  })
+              }}>
               <div className="inputs">
                 <input type="text" placeholder="Enter URL"
                   value={link}
@@ -45,8 +70,10 @@ const Linkmodal = () => {
                   onChange={setComment(event.target.value)}></textarea>
               </div>
               <div className="buttons">
-                <Button>Submit</Button>
-                <Button
+                <Button className="submit"
+                  variant="contained"
+                  color="primary">Submit</Button>
+                <Button clasName="cancel"
                   variant="contained"
                   color="secondary"
                   onClick={() => {
@@ -56,6 +83,48 @@ const Linkmodal = () => {
               </div>
             </form>
           </div>
+        </Modal>
+      ) : null}
+      {editModal ? (
+        <Modal className="edit-modal">
+        <h2>Edit link</h2>
+        <div className="form">
+          <form classname="post-form"
+            onSubmit={event => {
+              event.preventDefault();
+
+              const body = {
+                comment: linkComment,
+                clickCount: linkCount + 1
+              }
+
+              hitAPI("PATCH", `links/${linkID}`, body)
+                .then((data) => {
+                  console.log("update successful!")
+                  console.log(data)
+                  clear();
+                  setEditModal(false);
+                })
+            }}>
+            <div className="inputs">
+              <textarea placeholder="Enter a comment" rows="4"
+                value={linkComment}
+                onChange={setLinkComment(event.target.value)}></textarea>
+            </div>
+            <div className="buttons">
+              <Button className="submit"
+                variant="contained"
+                color="primary">Submit</Button>
+              <Button clasName="cancel"
+                variant="contained"
+                color="secondary"
+                onClick={() => {
+                  clear(); 
+                  setEditModal(false);                 
+                }}>Cancel</Button>
+            </div>
+          </form>
+        </div>
       </Modal>
       ) : null}
     </>
